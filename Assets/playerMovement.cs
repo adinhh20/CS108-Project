6f5d4c90ab25687dd2595ca04848cdbf49fc7ef7
Anh Dinh <adinhh20@gmail.com>
2023-10-16 14:57:38 -0700

    Project Files

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(dirX * 2f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector3(0, 20f, 0);
+            
+        }
+    }
+}
